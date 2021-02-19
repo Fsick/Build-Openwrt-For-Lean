97497dc1a9fe1b43ed1315f46c1ad1302c3d6b0f
Fsick <32356339+Fsick@users.noreply.github.com>
2021-02-20 00:20:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,13 +11,10 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-# Add helloword package
-git clone https://github.com/fw876/helloworld.git
-
-# Add MJ package
-git clone https://github.com/Fsick/openwrt-packages.git
+# Add MJ package feed source
+sed -i '$a src-git Fsick https://github.com/Fsick/openwrt-packages.git' feeds.conf.default
