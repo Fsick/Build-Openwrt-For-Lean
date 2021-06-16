49bbf3c7c19bd48893cfbada68a8bc3563c5d903
Fsick <32356339+Fsick@users.noreply.github.com>
2021-06-16 15:19:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add lienol package feed source
-sed -i '$a src-git Lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git Lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Add OpenWrt-Actions package feed source
 sed -i '$a src-git OpenWrt-Actions https://github.com/OpenWrt-Actions/helloworld' feeds.conf.default
