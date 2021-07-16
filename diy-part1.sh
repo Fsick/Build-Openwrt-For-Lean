49dd50addd70a56e915423677caeecadd85a197a
Fsick <32356339+Fsick@users.noreply.github.com>
2021-07-16 22:45:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,11 +13,15 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add lienol package feed source
-# sed -i '$a src-git Lienol https://github.com/Lienol/openwrt-package.git' feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
 
 # Add OpenWrt-Actions package feed source
-sed -i '$a src-git OpenWrt_Actions https://github.com/OpenWrt-Actions/helloworld.git' feeds.conf.default
+#echo 'src-git OpenWrt_Actions https://github.com/OpenWrt-Actions/helloworld.git' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+# Add Openwrt-Passwall package feed source
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # Add MJ package feed source
-sed -i '$a src-git Fsick https://github.com/Fsick/openwrt-packages.git' feeds.conf.default
+echo 'src-git Fsick https://github.com/Fsick/openwrt-packages.git' >>feeds.conf.default
